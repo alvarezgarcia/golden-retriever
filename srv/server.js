1532208b825f64dbc9f8f21b9ebb08765e60fe6b
Sebastián Alvarez <seba.alvarez16@gmail.com>
2017-12-26 15:23:59 -0300

    env files implementation

--- srv/server.js
+++ srv/server.js
@@ -1,7 +1,16 @@
+import dotenv from 'dotenv'
+
+if(dotenv.config().error || 
+	!process.env.SLACK_CLIENT_ID || 
+	!process.env.SLACK_CLIENT_SECRET) 
+	throw new Error('Please check your .env file exists and has SLACK_CLIENT_ID and SLACK_CLIENT_SECRET defined')
+
+
 import express from 'express'
 
 import { SimpleCache } from './cache/'
 import api from './api'
+
 
 const port = 9000
 const app = express()
