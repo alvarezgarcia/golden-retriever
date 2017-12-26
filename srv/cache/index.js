062215fb5ba6f310304439ccc8b6e015b901ab45
Sebastián Alvarez <seba.alvarez16@gmail.com>
2017-12-26 04:20:30 -0300

    Simple cache system, ready to extend in order to use other storage options

---
+++ srv/cache/index.js
@@ -0,0 +1,39 @@
+class Cache {
+
+	constructor() {
+		this.lastTimestamp = 0
+		this.cache = undefined
+	}
+
+	getLastTimestamp() {
+		return this.lastTimestamp
+	}
+
+	set(value) {
+		const timestamp = Math.round(new Date() / 1000)
+		this.lastTimestamp = timestamp
+
+		return this._set(value)
+	}
+
+	get() {
+		return this._get()
+	}
+
+}
+
+class SimpleCache extends Cache {
+
+	_set(value) {
+		this.cache = value
+		return this.cache
+	}
+
+	_get() {
+		return this.cache
+	}
+
+
+}
+
+export default SimpleCache
