43fc59e3f8f3b6565d11e00f9a70930ae10a1749
Sebastián Alvarez <seba.alvarez16@gmail.com>
2020-02-16 16:35:28 -0300

    Splitted public dir for static files

--- src/server.js
+++ src/server.js
@@ -3,14 +3,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const router = require('./services');
+const errorHandler = require('./errorHandler');
+
 const { port } = require('./config');
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
 
-app.use('/', (req, res, next) => res.json({a: 1}));
+app.use('/', router);
+app.use(errorHandler);
 
 const server = 
   app.listen(port).on('error', error => console.log('Express error', error));
