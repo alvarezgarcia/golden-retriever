90aef9d520cfd649d311f38d49aabdb9c79c9b09
Sebastián Alvarez <seba.alvarez16@gmail.com>
2020-02-24 03:21:50 -0300

    Grab new env variables for REdis and bitcoin price

--- src/config.js
+++ src/config.js
@@ -1,9 +1,21 @@
 'use strict';
 
 const {
-  PORT
+  PORT,
+  SLACK_OAUTH_URL,
+  SLACK_CLIENT_ID,
+  SLACK_CLIENT_SECRET,
+  REDIS_URL,
+  REDIS_TOPIC,
+  BITCOIN_PRICE_API_URL
 } = process.env;
 
 module.exports = {
-  port: parseInt(PORT, 10) || 6060
+  port: parseInt(PORT, 10) || 6060,
+  slackOauthUrl: SLACK_OAUTH_URL,
+  slackClientId: SLACK_CLIENT_ID,
+  slackClientSecret: SLACK_CLIENT_SECRET,
+  redisUrl: REDIS_URL,
+  redisTopic: REDIS_TOPIC,
+  bitcoinPriceApiUrl: BITCOIN_PRICE_API_URL
 };
